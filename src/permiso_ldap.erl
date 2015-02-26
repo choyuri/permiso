e8ea16d008f81755f6020a6846cf262c1fb8637a
Mariano Guerra <mariano@marianoguerra.org>
2015-02-26 16:59:38 +0000

    it's the other way around :(

--- src/permiso_ldap.erl
+++ src/permiso_ldap.erl
@@ -103,7 +103,7 @@ user_auth(#state{child=Child, handler=Mod, host=Host, port=Port,
           Username, Password) ->
     case eldap:open([Host], [{port, Port}]) of
         {ok, Pid} ->
-            DN = "uid=" ++ list_to_binary(Username) ++ "," ++ UserBase,
+            DN = "uid=" ++ binary_to_list(Username) ++ "," ++ UserBase,
             case eldap:simple_bind(Pid, DN, Password) of
                 ok ->
                     maybe_setup_user(Mod, Child, Username, Password, OnUserCreated),
